03b8dfb4bcea0af53bbff61f37984cf359879f7d
Jean-Jacques <r.bertoux@free.fr>
2020-11-23 11:32:41 +0100

    fini de jouer

--- src/fr/eql/ai108/groupeRMR/Import/TestMathilda.java
+++
@@ -1,7 +0,0 @@
-package fr.eql.ai108.groupeRMR.Import;
-
-public class TestMathilda extends Import {
-//Test commit Mathilda
-	//TEst 2 Thilda
-	//3e test thilda
-}
--- src/fr/eql/ai108/groupeRMR/Import/TestRomainD93leplusjeuneetpluspetit.java
+++
@@ -1,7 +0,0 @@
-package fr.eql.ai108.groupeRMR.Import;
-
-public class TestRomainD93leplusjeuneetpluspetit {
-	
-	//bzkejfbksjhdbvowdfv
-
-}
