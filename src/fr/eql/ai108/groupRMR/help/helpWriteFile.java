a82229fb19d37fcbbaf74d9dc6e549f1c9a9ef09
RomainD93 <romain.drogoul@gmail.com>
2020-11-24 12:31:51 +0100

    help

--- src/fr/eql/ai108/groupRMR/help/helpWriteFile.java
+++ src/fr/eql/ai108/groupRMR/help/helpWriteFile.java
@@ -1,5 +1,93 @@
 package fr.eql.ai108.groupRMR.help;
 
-public class helpWriteFile {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.RandomAccessFile;
 
+public class helpWriteFile {
+	private static File ecritureFichier(File lesVins) {
+
+		FileReader in=null;
+		BufferedReader br = null;
+		RandomAccessFile raf = null;
+		String ligne = "";
+		File retour = new File(lesVins.getParent()+"/VINSbin.DON");
+
+
+
+		if (retour.exists()) {
+			return retour ;
+		}
+		try {
+			in = new FileReader(lesVins);
+			br = new BufferedReader(in);
+			ligne = br.readLine();
+			raf = new RandomAccessFile(retour,"rw");
+			while(ligne!= null) {
+
+				byte[] tabBytes = ligne.getBytes();
+
+
+
+				int indexCol = 1;
+				int finLigne = tabBytes.length - 1;
+				int nbCaractere=0;
+
+
+				for (int i = 0; i < tabBytes.length; i++) {
+
+					//si on detecte un caractÃ¨re on Ã©crit le caractÃ¨re
+					//puis nbCaractere++
+					if (tabBytes[i]!='\t') {
+						raf.write(tabBytes[i]);
+						nbCaractere++;
+
+
+						// des que l'on detecte un \t on vÃ©rifie le nb d'espace manquants
+						// on les Ã©crits avec le RAF puis on change de colone
+					} else if ((tabBytes[i]=='\t') ) {
+						int nbespace= structure[indexCol]-nbCaractere;
+						for (int j = 0; j < nbespace; j++) {
+							raf.write('-');
+						}
+						indexCol++;
+						nbCaractere=0;
+
+
+						// une fois arrivÃ© au dernier caractÃ¨re du tableau
+						// on calcul le nombre d'espace manquant
+						//puis on les imprime dans le fichier
+					} else if (i == finLigne) {
+						for (int j = 0; j < (structure[indexCol]-nbCaractere); j++) {
+							raf.write('@');
+						}
+					}
+
+				}
+				ligne = br.readLine();
+			}	
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				br.close();
+				in.close();
+				raf.close();
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
+
+		}
+
+
+
+		return retour ;
+	}
 }
--- src/fr/eql/ai108/groupeRMR/model/InternDao.java
+++ src/fr/eql/ai108/groupeRMR/model/InternDao.java
@@ -10,6 +10,13 @@ import java.util.List;
 public class InternDao {
 	
 	private File file = new File("annuaire.bin");
+	
+	private Intern stringToIntern (String line) {
+		String[] infos = line.split(";");
+		Intern intern = new Intern(infos[0])
+		
+		return intern;
+	}
 
 	public List<Intern> getAll() {
 		List<Intern> interns = new ArrayList<Intern>();
