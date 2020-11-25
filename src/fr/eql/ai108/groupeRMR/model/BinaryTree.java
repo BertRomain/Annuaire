fe1457de5815630ca6f5bb1761467185557613db
Jean-Jacques <r.bertoux@free.fr>
2020-11-25 17:35:05 +0100

    ajout méthode de tri de l'arbre + refacto construction de l'arbre

--- src/fr/eql/ai108/groupeRMR/model/BinaryTree.java
+++ src/fr/eql/ai108/groupeRMR/model/BinaryTree.java
@@ -1,10 +1,17 @@
 package fr.eql.ai108.groupeRMR.model;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BinaryTree {
 
 	static Node root;
-	
+	static BinaryTree bt = new BinaryTree();
+	static List<Intern> interns = new ArrayList<Intern>();
+
 	//création arbre binaire 
 	class Node {
 		int value;
@@ -16,7 +23,7 @@ public class BinaryTree {
 			right = null;
 			left = null;
 		}
-		
+
 	}
 
 	private Node addRecursive(Node current, int value) {
@@ -41,49 +48,126 @@ public class BinaryTree {
 	}
 
 	private static BinaryTree createBinaryTree() {
-		BinaryTree bt = new BinaryTree();
-
+		
+		//TO DO insérer le fichier d'entrée ici 
 		bt.add(6);
 		bt.add(4);
 		bt.add(8);
-		bt.add(3);
+		bt.add(131);
 		bt.add(5);
 		bt.add(7);
-		bt.add(9);
-		
+		bt.add(10);
+		bt.add(23);
+		bt.add(54);
+		bt.add(81);
+
 		return bt;
 	}
-	
+
 	//recherche arbre binaire 
 	private static boolean containsNodeRecursive(Node current, int value) {
-	    if (current == null) {
-	        return false;
-	    }
-	    if (value == current.value) {
-	        return true;
-	    }
-	    return value < current.value
-	      ? containsNodeRecursive(current.left, value)
-	      : containsNodeRecursive(current.right, value);
+		if (current == null) {
+			return false;
+		}
+		if (value == current.value) {
+			return true;
+		}
+		return value < current.value
+				? containsNodeRecursive(current.left, value)
+						: containsNodeRecursive(current.right, value);
 	}
-	
+
 	public static boolean containsNode(int value) {
-	    return containsNodeRecursive(root, value);
+		return containsNodeRecursive(root, value);
+	}
+
+
+	//Suppression 
+
+	private static int findSmallestValue(Node root) {
+		return root.left == null ? root.value : findSmallestValue(root.left);
+	}
+
+	private static Node deleteRecursive(Node current, int value) {
+		if (current == null) {
+			return null;
+		}
+
+		if (value == current.value) {
+			// Case 1: no children
+			if (current.left == null && current.right == null) {
+				return null;
+			}
+
+			// Case 2: only 1 child
+			if (current.right == null) {
+				return current.left;
+			}
+
+			if (current.left == null) {
+				return current.right;
+			}
+
+			// Case 3: 2 children
+			int smallestValue = findSmallestValue(current.right);
+			current.value = smallestValue;
+			current.right = deleteRecursive(current.right, smallestValue);
+			return current;
+		}
+		if (value < current.value) {
+			current.left = deleteRecursive(current.left, value);
+			return current;
+		}
+
+		current.right = deleteRecursive(current.right, value);
+		return current;
+	}
+
+	public static void delete(int value) {
+		root = deleteRecursive(root, value);
 	}
 	
-//	@Test
-//	public void givenABinaryTree__WhenAddingElements__ThenTreeContainsThoseElements() {
-//	    BinaryTree bt = createBinaryTree();
+	public static void traverseInOrder(Node node) {
+		if (node != null) {
+			traverseInOrder(node.left);
+			System.out.print(" " + node.value);
+			//methode RAF
+			traverseInOrder(node.right);
+		}
+	}
+
+//	public static void writeFile(Node mainNode)
+//	{
+//		FileOutputStream outputStream = null;
+//		PrintWriter printWriter = null;
+//		String path = "c:\\DossierAI108\\bt.bin";
+//		try	{
+//			outputStream = new FileOutputStream(path);
+//			printWriter = new PrintWriter(outputStream); 
 //
-//	    assertTrue(bt.containsNode(6));
-//	    assertTrue(bt.containsNode(4));
+//			write(mainNode, printWriter);
 //
-//	    assertFalse(bt.containsNode(1));
+//			printWriter.flush();
+//
+//		} catch(IOException e) {
+//			System.out.println("An error occured");
+//			printWriter.close();
+//		}
 //	}
-	
-	
+
+	public static void write(Node mainNode, PrintWriter w)
+	{
+		if(mainNode != null){
+			write(mainNode.left, w);
+			w.print(mainNode);
+			write(mainNode.right, w);	
+		}
+	}
+
+
 	public static void main(String[] args) {
 		createBinaryTree();
-		System.out.println(containsNode(1));
-	}
+		System.out.println(containsNode(3));
+		traverseInOrder(root);
+			}
 }
