8d0e1f6d5f1865ca0f8cf182cd3e455d20b05637
RomainD93 <romain.drogoul@gmail.com>
2020-11-26 13:54:12 +0100

    Commit 3

--- src/fr/eql/ai108/groupeRMR/ihm/FirstPaneWelcome.java
+++ src/fr/eql/ai108/groupeRMR/ihm/FirstPaneWelcome.java
@@ -22,6 +22,7 @@ public class FirstPaneWelcome extends BorderPane {
 		setBottom(hBoxAdminAccessBtn);		
 		setPrefSize(1919, 1008);
 		
+		
 		btnWelcome.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
