ce705b1451b51c8fcb4f3e3a179640c039c623b5
Jean-Jacques <r.bertoux@free.fr>
2020-11-26 11:23:10 +0100

    + nodeToString

--- src/fr/eql/ai108/groupeRMR/model/BinaryTreeInternWriteFile.java
+++ src/fr/eql/ai108/groupeRMR/model/BinaryTreeInternWriteFile.java
@@ -2,7 +2,6 @@ package fr.eql.ai108.groupeRMR.model;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -13,12 +12,39 @@ public class BinaryTreeInternWriteFile {
 	private static int lengthOfRecord = 260;
 	private static int index = 0;
 	static Node root;
+	static BinaryTreeInternWriteFile bt = new BinaryTreeInternWriteFile();
 
-	private static File writeFile(File fileInterns) {
-		File sortedInterns = new File(fileInterns.getParent()+ "/sortedInterns.bin");
-		if (sortedInterns.exists()) {
-			return sortedInterns;
+	private static void writeFile(File interns, Node current) {
+
+		RandomAccessFile raf = null;
+
+		try {
+			byte[] b= nodeToByte(current);
+			raf = new RandomAccessFile(interns, "w");
+			raf.write(b);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				raf.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+	}
+
+
+	public static void traverseInOrder(Node node) {
+		if (node != null) {
+			traverseInOrder(node.left);
+			writeFile(interns, node);
+			traverseInOrder(node.right);
+			writeFile(interns, node);
+		}
+	}
+
+
+	private static void readFile(File fileInterns) {
 
 		FileReader in = null;
 		BufferedReader br = null;
@@ -28,14 +54,13 @@ public class BinaryTreeInternWriteFile {
 			in = new FileReader(fileInterns);
 			br = new BufferedReader(in);
 			String line;
-			raf = new RandomAccessFile(sortedInterns, "rw");
+			raf = new RandomAccessFile(fileInterns, "r");
 			long offset = index*lengthOfRecord;
 
 			while (br.ready()) {
 				raf.seek(offset);
 				line = br.readLine();
-				byte[] internsTab = new byte[lengthOfRecord];
-				raf.write(internsTab);
+				bt.add(line);
 				index ++;
 			}
 
@@ -50,8 +75,6 @@ public class BinaryTreeInternWriteFile {
 				e.printStackTrace();
 			}
 		}
-
-		return sortedInterns;
 	}
 
 	class Node {
@@ -65,17 +88,47 @@ public class BinaryTreeInternWriteFile {
 			left = null;
 		}
 	}
+
+	private static String nodeToString(Node origin) {
+		String nodeValue = origin.toString();
+		return nodeValue;
+	}
 	
-	private Node addRecursive(Node current, String internString) {
+
+	private static byte[] nodeToByte(Node origin) {
+		String nodeValue = origin.toString();
+		byte[] b = new byte[nodeValue.length()];
+		return b;
+	}
+	
+	public static Node addRecursive(Node current, String internString) {
+
 		if (current == null) {
 			return new Node(internString);
 		}
-		if (internString.) {
-			
+		if (internString.compareTo(nodeToString(current)) > 0 ) {
+			current.left = addRecursive(current.left, internString);
+		} else if (internString.compareTo(nodeToString(current)) < 0 ) {
+			current.right = addRecursive(current.right, internString);
+		} else {
+			return current;
 		}
-		
 		return current;
 	}
+
+	public static void add(String internString) {
+		root = addRecursive(root, internString);
+	}
+
+	public static BinaryTreeInternWriteFile createBinaryTree() {
+
+		readFile(interns);
+		
+		return bt;
+
+	}
+
+
 
 	public static void main(String[] args) {
 
