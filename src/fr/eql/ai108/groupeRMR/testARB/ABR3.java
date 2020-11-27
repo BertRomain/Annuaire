f8a78d7fb11b2c55709ad300f1db4cbc0b1682c7
Mathilda <mathilda.landreau@wanadoo.fr>
2020-11-27 14:17:08 +0100

    Commit ABR3

---
+++ src/fr/eql/ai108/groupeRMR/testARB/ABR3.java
@@ -0,0 +1,88 @@
+package fr.eql.ai108.groupeRMR.testARB;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+public class ABR3 {
+	private Node racine;
+
+
+	public ABR3() {}
+
+	public ABR3(String nomFichier)  throws IOException {
+		construire(nomFichier);	
+	}
+
+	public void inserer(String mot) {
+		racine = inserer(racine, mot);
+	}
+
+	public Node inserer(Node rac, String mot ) {
+		if (rac == null) {
+			Node nouveau = new Node(mot);
+			return nouveau;
+		}
+		if (mot.compareTo(rac.getMot()) < 0) rac.setFg(inserer(rac.getFg(), mot));
+		else if (mot.compareTo(rac.getMot()) == 0) rac.setNbOcc(rac.getNbOcc() + 1);
+		else rac.setFd(inserer(rac.getFd(), mot));
+		return rac;
+	}
+
+	public void construire(String nomFichier) throws IOException {
+		Scanner lecteur = new Scanner(new File(nomFichier));
+
+		while(lecteur.hasNext()) inserer(lecteur.next());
+	}
+
+	public int hauteur() {
+		return hauteur(racine);
+	}
+
+	private int hauteur(Node rac) {
+		if (rac == null) return -1;
+		int hg = hauteur(rac.getFg());
+		int hd = hauteur(rac.getFd());
+		if (hg < hd) return hd + 1;
+		return hg + 1;
+	}
+
+	public void ecrireListeTriee() {
+		ecrireListeTriee(racine);
+	}
+	public 		RandomAccessFile raf = null;
+	public String line = "";
+	private int lengthOfRecord = 260;
+	int index = 0;
+	int offset = 0;
+	public void ecrireListeTriee(Node rac) {
+		offset = index*lengthOfRecord;
+		if (rac != null) {
+			ecrireListeTriee(rac.getFg());	
+			try {
+				raf = new RandomAccessFile("C:/Users/Formation/Desktop/test.bin", "rw");							
+				while(raf.readLine() != null) {
+				raf.readLine();					
+				}
+				raf.seek(offset);
+				raf.writeBytes(rac.getMot()+ "\r");
+				index ++;
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		System.out.println(rac.getMot());
+			ecrireListeTriee(rac.getFd());
+		}		
+	}
+
+	public void ecrireFichierTrie(Node rac) {
+		if (rac != null) {
+			ecrireFichierTrie(rac.getFg());
+//			System.out.println(rac.getMot() + " (" + rac.getNbOcc() + " fois)");
+			ecrireFichierTrie(rac.getFd());
+		}		
+	}
+}
---
+++ src/fr/eql/ai108/groupeRMR/testARB/TestFileIntern3.java
@@ -0,0 +1,36 @@
+package fr.eql.ai108.groupeRMR.testARB;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TestFileIntern3 {
+	static File interns = new File("C:/Users/Formation/Desktop/intern.bin");
+public static void main(String[] args) {
+	ABR3 abr = new ABR3();
+	FileReader in = null;
+	BufferedReader br = null;
+	String line = null;
+	try {
+		in = new FileReader(interns);
+		br = new BufferedReader(in);
+		
+		while(br.ready()) {
+			line = br.readLine();
+			abr.inserer(line);
+			
+		}
+		System.out.println("L'arbre est de hauteur " + abr.hauteur());
+	abr.ecrireListeTriee();
+		
+		
+		
+		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+}
+}
